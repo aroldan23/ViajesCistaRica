983ab0bcba4c4a535caf45ee99c40adb6a19343c
aroldan23 <antoroldan23@gmail.com>
2022-08-17 23:11:20 -0600

    Inicio

---
+++ PHP/cliente.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/Style_cliente.css">
+    <title>Menu Cliente</title>
+</head>
+<body>
+    <aside>
+        <p> Menu de clientes </p>
+        <a href="javascript:void(0)">
+          <i class="fa fa-user-o" aria-hidden="true"></i>
+          Monedero electronico
+        </a>
+        <a href="javascript:void(0)">
+          <i class="fa fa-laptop" aria-hidden="true"></i>
+          Rutas
+        </a>
+        <a href="javascript:void(0)">
+          <i class="fa fa-clone" aria-hidden="true"></i>
+          Gastos
+        </a>
+        <a href="javascript:void(0)">
+          <i class="fa fa-star-o" aria-hidden="true"></i>
+         Viajes Realizados
+        </a>
+        <a href="../PHP/cerrarSesion.php">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+          Salir
+        </a>
+    </aside>
+</body>
+</html>
